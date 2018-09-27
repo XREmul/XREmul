7c142b04da6d227f6f9dc313da2379f990cc011c
Jaewon Choi <jaewon.james.choi@gmail.com>
2018-09-27 22:41:15 +0900

    Add HMDRecorder for Fove

---
+++ Assets/Scripts/XREmul/Recorders/HMDRecorder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class HMDRecorder : MonoBehaviour {
+    public bool IsRecording = false;
+
+    private float timer_ = 0.0f;
+
+    class RotationLog
+    {
+        public float Timer;
+        public Quaternion Rotation;
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2},{3},{4}",
+                Timer, Rotation.x, Rotation.y, Rotation.z, Rotation.z);
+        }
+    }
+
+    private List<RotationLog> rotationLogs_ = new List<RotationLog>();
+    
+	void Update()
+    {
+        if (!IsRecording)
+            return;
+
+        rotationLogs_.Add(new RotationLog
+        {
+            Timer = timer_,
+            Rotation = transform.rotation
+        });
+
+        timer_ += Time.deltaTime;
+    }
+
+    public void Dump(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path, true))
+        {
+            foreach (RotationLog log in rotationLogs_)
+            {
+                writer.WriteLine(log.ToString());
+            }
+
+            rotationLogs_.Clear();
+        }
+    }
+}
---
+++ Assets/Scripts/XREmul/Recorders/HMDRecorderEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HMDRecorder))]
+public class HMDRecorderEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var hmdRecorder = (HMDRecorder)target;
+
+        if (GUILayout.Button("Save to File"))
+        {
+            var path = EditorUtility.SaveFilePanel("Save logs", "", "", ".csv");
+
+            hmdRecorder.Dump(path);
+        }
+    }
+}
